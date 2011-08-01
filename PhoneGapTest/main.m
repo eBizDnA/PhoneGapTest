5c8f52d9a151a3973da93ededc0f8339ba8bc263
Daniel Chitiva <admin@eBizDnAMacBookPro.local>
2011-08-01 18:17:08 -0400

    Initial Commit

---
+++ PhoneGapTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PhoneGapTest
+//
+//  Created by admin on 8/1/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
